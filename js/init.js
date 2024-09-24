2e5d0bf7d62b45c785e6ef82c761f7c01528b04f
sumin0126 <ktnals2@naver.com>
2024-09-24 18:39:29 +0900

    feat: 새로운 컬럼에 드래그 이벤트 추가

--- js/init.js
+++ js/init.js
@@ -1,8 +1,7 @@
 import { createNewCard } from './card.js';
 import { createNewColumn, openDeleteColumnModal } from './column.js';
 import { closeModal, openInputModal } from './modal.js';
-import { dragStart, dragEnter, dragLeave, dragOver, dropCard } from './drag.js';
-
+import { addColumnDragEvents } from './column.js';
 /* 새카드 생성해주는 이벤트 */
 const addBtns = document.querySelectorAll('.add-card');
 addBtns.forEach((btn) => btn.addEventListener('click', createNewCard));
@@ -13,11 +12,7 @@ deleteBtns.forEach((btn) => btn.addEventListener('click', openDeleteColumnModal)
 
 const columns = document.querySelectorAll('.todo-column');
 columns.forEach((column) => {
-  column.addEventListener('dragstart', dragStart); // 드래그 시작할 때
-  column.addEventListener('dragenter', dragEnter); // 카드가 컬럼에 들어올 때
-  column.addEventListener('dragleave', dragLeave); // 카드가 컬럼을 떠날 때
-  column.addEventListener('dragover', dragOver); // 카드가 컬럼 위에 있을 때
-  column.addEventListener('drop', dropCard); // 카드가 컬럼에 드롭될 때
+  addColumnDragEvents(column);
 });
 
 const handleConfirmClick = () => {
